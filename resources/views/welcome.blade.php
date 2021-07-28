aab5d58bb6ff145db437edb9ee54473149a24668
dedelukman <dlukmanul.h@gmail.com>
2021-07-28 21:42:17 +0700

    add nav logo login, change logo

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,3 +1,22 @@
 <x-guest-layout>
-    <x-application-logo></x-application-logo>
+    <div class="flex flex-col bg-indigo-900 w-full h-screen">
+        <nav class="flex pt-5 justify-between container mx-auto text-indigo-200">
+            <a href="/">
+                <x-application-logo class="w-16 h-16 fill-current"></x-application-logo>
+            </a>
+
+            <div class="flex justiy-end">
+                @auth
+                    <a href="{{ route('dashboard') }}">Dashboard</a>
+                @else
+                    <a href="{{ route('login') }}">Login</a>
+                @endauth
+            </div>
+        </nav>
+
+    </div>
+
+    <div class="flex flex-col bg-pink-400 w-full h-screen">
+
+    </div>
 </x-guest-layout>
