3b2c84d90e8a3835b3bc92d62d0d4aa1844fa597
dedelukman <dlukmanul.h@gmail.com>
2021-07-28 20:26:34 +0700

    edit model subscriber

--- app/Models/Subscriber.php
+++ app/Models/Subscriber.php
@@ -8,4 +8,22 @@ use Illuminate\Database\Eloquent\Model;
 class Subscriber extends Model
 {
     use HasFactory;
+ /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'email',
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
 }
+
