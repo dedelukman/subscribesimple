bff2e1094f346a117d07f8fd31a7c0212ce9e50f
dedelukman <dlukmanul.h@gmail.com>
2021-07-29 16:45:37 +0700

    save to database & set reset input form

--- app/Http/Livewire/LandingPage.php
+++ app/Http/Livewire/LandingPage.php
@@ -3,13 +3,17 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
+use App\Models\Subscriber;
 
 class LandingPage extends Component
 {
     public $email;
 
     public function subscribe(){
-        \Log::debug($this->email);
+        $subscriber = Subscriber::create([
+            'email' => $this->email,
+        ]);
+        $this->reset('email');
     }
 
     public function render()
