e2bf6f83f55b4f7d687f0e1dd5b36881e18137ef
dedelukman <dlukmanul.h@gmail.com>
2021-07-30 09:35:19 +0700

    add search function

--- app/Http/Livewire/Subscribers.php
+++ app/Http/Livewire/Subscribers.php
@@ -10,12 +10,14 @@ class Subscribers extends Component
 {
     use WithPagination;
 
+    public $search;
+
     public function delete(Subscriber $subscriber){
         $subscriber->delete();
     }
 
     public function render()
     {
-        return view('livewire.subscribers')->with(['subscribers' => Subscriber::paginate(10)]);
+        return view('livewire.subscribers')->with(['subscribers' => Subscriber::where('email','like',"%{$this->search}%")->paginate(10)]);
     }
 }
